c4281839edabf6bbc33fca786835edefbed5d807
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-16 11:44:13 +0800

    feat: Added imports and code to  home.ts

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  clerkMessage = "Welcome to our PokéMart digital portal! Feel free to switch between any of our active retail stock rows on the display panel above.";
+}

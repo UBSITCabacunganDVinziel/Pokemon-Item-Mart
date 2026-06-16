98056f920fcfe6ef966638d6ed3068a0aee111f0
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-16 12:07:45 +0800

    feat: Added the routes on the app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PokeBall } from './poke-ball/poke-ball';
+import { HealingItem } from './healing-item/healing-item';
+import { StatusHealing } from './status-healing/status-healing';
+import { EscapeItem } from './escape-item/escape-item';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'pokeball', component: PokeBall },
+    { path: 'healingitem', component: HealingItem },
+    { path: 'statushealing', component: StatusHealing},
+    { path: 'escapeitem', component: EscapeItem}
+];

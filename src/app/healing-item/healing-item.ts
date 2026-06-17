eead4fbcaa008c020e102272662e82d5261753dc
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-17 08:59:45 +0800

    feat: Added the TypeScript code for the healing-item.ts

--- src/app/healing-item/healing-item.ts
+++ src/app/healing-item/healing-item.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { MartService } from '../pokemart-service';
+import { CartService } from '../cart-service';
+
 
 @Component({
   selector: 'app-healing-item',
+  standalone: true,
   imports: [],
   templateUrl: './healing-item.html',
   styleUrl: './healing-item.css',
 })
-export class HealingItem {}
+export class HealingItem {
+  private martService = inject(MartService);
+  private cartService = inject(CartService);
+
+  filteredItems = computed(() => 
+    this.martService.items().filter(i => i.category === 'Healing item')
+  );
+
+  onAddItem(item: MartItem) {
+    this.cartService.addToCart(item);
+  }
+}

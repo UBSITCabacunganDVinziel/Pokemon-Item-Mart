ff973bb706b335e88f72d080e71c8ee37a6ea690
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-17 08:59:17 +0800

    feat: Added the TypeScript code for the poke-ball.ts

--- src/app/poke-ball/poke-ball.ts
+++ src/app/poke-ball/poke-ball.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
 import { MartService } from '../pokemart-service';
 import { CartService } from '../cart-service';
-import { PokemonItem } from '../pokemon-item.model';
+
 
 @Component({
   selector: 'app-poke-ball',
+  standalone: true,
   imports: [],
   templateUrl: './poke-ball.html',
   styleUrl: './poke-ball.css',

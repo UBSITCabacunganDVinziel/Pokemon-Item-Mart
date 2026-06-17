338154ca7332afb9cb9a20d56c665ea06e691038
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-17 09:41:01 +0800

    feat: Fixed minor error

--- src/app/cart-service.ts
+++ src/app/cart-service.ts
@@ -10,32 +10,39 @@ export class CartService {
 
   readonly cart = this.cartState.asReadonly();
 
-  readonly cartTotal = computed(() => 
-    this.cartState().reduce((sum, current) => sum + (current.item.price * current.quantity), 0)
+  readonly cartCount = computed(() => 
+    this.cartState().reduce((total, currentItem) => total + currentItem.quantity, 0)
   );
 
-  readonly cartCount = computed(() => 
-    this.cartState().reduce((count, current) => count + current.quantity, 0)
+  readonly cartTotal = computed(() => 
+    this.cartState().reduce((totalPrice, currentItem) => totalPrice + (currentItem.item.price * currentItem.quantity), 0)
   );
 
   addToCart(item: PokemonItem) {
     this.cartState.update(currentCart => {
-      const existingIdx = currentCart.findIndex(c => c.item.id === item.id);
-      if (existingIdx > -1) {
+      const existingItemIndex = currentCart.findIndex(cartItem => cartItem.item.id === item.id);
+
+      if (existingItemIndex > -1) {
+
         const updatedCart = [...currentCart];
-        updatedCart[existingIdx] = {
-          ...updatedCart[existingIdx],
-          quantity: updatedCart[existingIdx].quantity + 1
+        updatedCart[existingItemIndex] = {
+          ...updatedCart[existingItemIndex],
+          quantity: updatedCart[existingItemIndex].quantity + 1
         };
         return updatedCart;
       }
+
       return [...currentCart, { item, quantity: 1 }];
     });
   }
 
   removeFromCart(itemId: number) {
     this.cartState.update(currentCart => 
-      currentCart.filter(c => c.item.id !== itemId)
+      currentCart.filter(cartItem => cartItem.item.id !== itemId)
     );
+  }
+
+  clearCart() {
+    this.cartState.set([]);
   }
 }

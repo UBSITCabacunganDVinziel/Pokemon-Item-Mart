bc8000c90a865da173145ca795b7b982de65d3e4
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-17 09:00:37 +0800

    feat: Added the TypeScript code for the escape-item.ts

--- src/app/escape-item/escape-item.ts
+++ src/app/escape-item/escape-item.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { MartService } from '../pokemart-service';
+import { CartService } from '../cart-service';
+
 
 @Component({
   selector: 'app-escape-item',
+  standalone: true,
   imports: [],
   templateUrl: './escape-item.html',
   styleUrl: './escape-item.css',
 })
-export class EscapeItem {}
+export class EscapeItem {
+  private martService = inject(MartService);
+  private cartService = inject(CartService);
+
+  filteredItems = computed(() => 
+    this.martService.items().filter(i => i.category === 'Escape Item')
+  );
+
+  onAddItem(item: MartItem) {
+    this.cartService.addToCart(item);
+  }
+}

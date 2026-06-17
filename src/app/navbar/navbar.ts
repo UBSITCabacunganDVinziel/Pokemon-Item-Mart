c09b8f7e439d20dc57bc5373934bf71e779bf9f4
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-17 09:07:38 +0800

    feat: Added import to the navbar.ts

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from './cart-service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  cartService = inject(CartService)
+}

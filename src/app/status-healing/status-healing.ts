cf47804a6e7eb44076cff81aa7a41ca38e663ec9
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-17 09:00:15 +0800

    feat: Added the TypeScript code for the status-healing.ts

--- src/app/status-healing/status-healing.ts
+++ src/app/status-healing/status-healing.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { MartService } from '../pokemart-service';
+import { CartService } from '../cart-service';
+
 
 @Component({
   selector: 'app-status-healing',
+  standalone: true,
   imports: [],
   templateUrl: './status-healing.html',
   styleUrl: './status-healing.css',
 })
-export class StatusHealing {}
+export class StatusHealing {
+  private martService = inject(MartService);
+  private cartService = inject(CartService);
+
+  filteredItems = computed(() => 
+    this.martService.items().filter(i => i.category === 'Status Healing')
+  );
+
+  onAddItem(item: MartItem) {
+    this.cartService.addToCart(item);
+  }
+}

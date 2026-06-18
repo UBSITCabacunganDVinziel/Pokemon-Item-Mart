d5cda5c784a4bc82214626b7943cb64c8af4a4e8
cabacungan_v <cabacungan_v@f112.ub.lan>
2026-06-18 08:46:20 +0800

    feat: Added code to the TypeScript of item-card

--- src/app/item-card/item-card.ts
+++ src/app/item-card/item-card.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonItem } from '../pokemon-item.model';
 
 @Component({
   selector: 'app-item-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './item-card.html',
   styleUrl: './item-card.css',
 })
-export class ItemCard {}
+export class ItemCard {
+  item = input.required<PokemonItem>();
+  purchase = output<PokemonItem>();
+
+  onPurchaseClick() {
+    this.purchase.emit(this.item());
+  }
+}
